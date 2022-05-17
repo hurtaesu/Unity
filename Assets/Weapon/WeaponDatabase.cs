79339fedf35483e135dfdee1ae3fe6ef1c9d42c0
hurtaesu <102512411+hurtaesu@users.noreply.github.com>
2022-05-17 11:48:13 +0900

    Weapon Database 추가

---
+++ Assets/Weapon/WeaponDatabase.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDatabase : MonoBehaviour
+{
+    public bool Sword; //칼
+    public bool Wand; //마법지팡이
+    public bool Shield; //방패
+
+    public int Damage; //공격력
+    public float AttackSpeed; //공속
+}
